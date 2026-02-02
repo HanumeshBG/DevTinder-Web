7d49f93ac5bdbd615314045032efc9f03915bc2c
HanumeshBG <hanumeshbg042@gmail.com>
2026-02-02 16:51:56 +0530

    changes related to reading the base URL.

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,4 @@
 // export const BASE_URL = "http://localhost:3000";
-export const BASE_URL = "/api"; // Updated for production deployment
+// export const BASE_URL = "/api"; // Updated for production deployment
+
+export const BASE_URL = location.hostname === "localhost" ? "http://localhost:3000" : "/api";
